9ae88fa3954dcc15dbb0a8ca2a2e2926b013c092
Abdulquadri  Adegbiji <codehunter@Gran-Jefe-MacBook-Pro.local>
2025-03-17 01:34:15 +0100

    Add CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
-config(); // Load .env manually
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Allow CORS from all origins
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Only needed if you are sending cookies or authentication headers
+  });
+
   await app.listen(3000);
 }
 bootstrap();
